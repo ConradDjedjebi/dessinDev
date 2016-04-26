e42c1b7e5947f95f3c699404923fab97e96cf2af
duhamean <antoine.duhamel@reseau.eseo.fr>
2016-04-26 15:02:12 +0200

    Ajout partiel de la classe VueForme

---
+++ src/fr/eseo/gpi/beanartist/vue/geom/VueForme.java
@@ -0,0 +1,19 @@
+package fr.eseo.gpi.beanartist.vue.geom;
+
+import java.awt.*;
+
+/**
+ * @author duhamean
+ * @date 26/04/16
+ * @project gpi_binome
+ */
+public class VueForme {
+    private static final Color COULEUR_LIGNE_PAR_DÉFAULT = Color.BLACK;
+
+    public VueForme(Forme forme, boolean rempli) {
+        this(forme, COULEUR_LIGNE_PAR_DÉFAULT, rempli);
+        }
+    public VueForme(Forme forme, Color couleurLigne, boolean rempli) {
+        //
+    }
+}
