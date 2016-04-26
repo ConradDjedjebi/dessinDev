8a98ff9d6b218408891091f8e28d6c183ce5fe65
duhamean <antoine.duhamel@reseau.eseo.fr>
2016-04-26 11:30:48 +0200

    Implémentation des classes d'UI

--- src/fr/eseo/gpi/beanartist/vue/ui/FenêtreBeAnArtist.java
+++ src/fr/eseo/gpi/beanartist/vue/ui/FenêtreBeAnArtist.java
@@ -1,22 +1,53 @@
 package fr.eseo.gpi.beanartist.vue.ui;
 
+import javax.swing.JFrame;
+import java.awt.Color;
+
 /**
- * Created by Elphege on 26/04/2016.
+ * @author Elphege
+ * @date 26/04/2016
+ * @project gpi_binome
  */
-public class FenêtreBeAnArtist {
-    final static String TITRE_PAR_DÉFAUT = "Fenêtre_Par_DÉFAUT";
+public class FenêtreBeAnArtist extends JFrame {
+    private final static String TITRE_PAR_DÉFAUT = "Fenêtre Be an artist";
 
     private int largeur;
     private int hauteur;
-    private String Couleur_Fond;
-    private String Couleur_Ligne;
+    private Color couleurFond;
+    private Color couleurLigne;
     private String titre;
+    private PanneauDessin panneauDessin;
 
     public FenêtreBeAnArtist(){
-        titre = TITRE_PAR_DÉFAUT.
-        largeur = PanneauDessin.LARGEUR_PAR_DÉFAUT;
-        hauteur = PanneauDessin.HAUTEUR_PAR_DÉFAUT;
-        Couleur_Fond = PanneauDessin.COULEUR_FOND_PAR_DÉFAUT;
-        Couleur_Ligne = PanneauDessin.COULEUR_LIGNE_PAR_DÉFAUT;
+        this(TITRE_PAR_DÉFAUT, PanneauDessin.LARGEUR_PAR_DÉFAUT, PanneauDessin.HAUTEUR_PAR_DÉFAUT, PanneauDessin.COULEUR_FOND_PAR_DÉFAUT);
+    }
+
+    public FenêtreBeAnArtist(String titre){
+        this(titre, PanneauDessin.LARGEUR_PAR_DÉFAUT, PanneauDessin.HAUTEUR_PAR_DÉFAUT, PanneauDessin.COULEUR_FOND_PAR_DÉFAUT);
+    }
+
+    public FenêtreBeAnArtist(int largeur, int hauteur){
+        this(largeur, hauteur, PanneauDessin.COULEUR_FOND_PAR_DÉFAUT);
+    }
+
+    public FenêtreBeAnArtist(int largeur, int hauteur, Color couleurFond){
+        this(TITRE_PAR_DÉFAUT, largeur, hauteur, couleurFond);
+    }
+
+    public FenêtreBeAnArtist(String titre, int largeur, int hauteur, Color fond){
+        this.titre = titre;
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.couleurFond = fond;
+        this.couleurLigne = PanneauDessin.COULEUR_LIGNE_PAR_DÉFAUT;
+        this.panneauDessin = new PanneauDessin(largeur, hauteur);
+    }
+
+    public void setPanneauDessin(PanneauDessin newPanneauDessin) {
+        this.panneauDessin = newPanneauDessin;
+    }
+
+    public PanneauDessin getPanneauDessin() {
+        return this.panneauDessin;
     }
 }
--- src/fr/eseo/gpi/beanartist/vue/ui/PanneauDessin.java
+++ src/fr/eseo/gpi/beanartist/vue/ui/PanneauDessin.java
@@ -1,11 +1,29 @@
 package fr.eseo.gpi.beanartist.vue.ui;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
- * Created by Elphege on 26/04/2016.
+ * @author Elphege
+ * @date 26/04/2016
+ * @project gpi_binome
  */
-public class PanneauDessin {
+public class PanneauDessin extends JPanel {
     final static int LARGEUR_PAR_DÉFAUT = 1;
     final static int HAUTEUR_PAR_DÉFAUT = 1;
-    final static String COULEUR_FOND_PAR_DÉFAUT = "blue";
-    final static String COULEUR_LIGNE_PAR_DÉFAUT = "noir";
+    final static Color COULEUR_FOND_PAR_DÉFAUT = Color.BLUE;
+    final static Color COULEUR_LIGNE_PAR_DÉFAUT = Color.BLACK;
+
+    private FenêtreBeAnArtist fenêtre;
+
+    public PanneauDessin (int largeur, int hauteur) {
+        this(largeur, hauteur, COULEUR_FOND_PAR_DÉFAUT);
+        }
+
+    public PanneauDessin (int largeur, int hauteur, Color couleurFond) {
+        // methode
+    }
+
+    void setFenêtre (FenêtreBeAnArtist fen) {}
+
 }
