f1af328c7f2ead0f5f83a8f1d37f12618f68a8a1
duhamean <antoine.duhamel@reseau.eseo.fr>
2016-04-28 12:00:25 +0200

    Validation du 3.4.2 en cours (En attante de réponse de l'AssCen)

---
+++ src/fr/eseo/gpi/beanartist/controleur/outils/OutilForme.java
@@ -0,0 +1,34 @@
+package fr.eseo.gpi.beanartist.controleur.outils;
+
+import fr.eseo.gpi.beanartist.modele.geom.Forme;
+import fr.eseo.gpi.beanartist.vue.geom.VueForme;
+import fr.eseo.gpi.beanartist.vue.ui.PanneauDessin;
+
+import java.awt.event.MouseEvent;
+
+/**
+ * @author duhamean
+ * @date 28/04/16
+ * @project gpi_binome
+ */
+public abstract class OutilForme extends Outil {
+    Forme forme;
+
+    public OutilForme(PanneauDessin panneauDessin) {
+        super(panneauDessin);
+    }
+
+    @Override
+    public void mouseClicked (MouseEvent e){
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        super(e);
+    }
+
+    public void créerVueForme() {
+        new VueForme(forme);
+    }
+}
