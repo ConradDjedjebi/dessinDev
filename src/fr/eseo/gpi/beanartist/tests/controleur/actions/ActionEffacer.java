908d05594b846e6c9752c83fcc7e8aa1ae9b1bc8
duhamean <antoine.duhamel@reseau.eseo.fr>
2016-05-02 18:33:59 +0200

    Ajout de la classe ActionEffacer

---
+++ src/fr/eseo/gpi/beanartist/tests/controleur/actions/ActionEffacer.java
@@ -0,0 +1,24 @@
+package fr.eseo.gpi.beanartist.tests.controleur.actions;
+
+import fr.eseo.gpi.beanartist.vue.ui.FenêtreBeAnArtist;
+
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+/**
+ * @author duhamean
+ * @date 02/05/16
+ * @project gpi_binome
+ */
+public class ActionEffacer extends AbstractAction {
+    private static final String NOM_ACTION = "";
+
+    public ActionEffacer (FenêtreBeAnArtist fenetre) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
