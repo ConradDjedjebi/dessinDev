392a0701fe838fa832df88d2b0be900c47521a61
Djedjebi <djelphege@gmail.com>
2016-05-15 18:54:34 +0200

    Un bug s'est incrusté dans le dernier commit.
    (ActionChoisirCouleur validée
    setHauteur() et setLargeur() corrigées pour le Tracé)

--- src/fr/eseo/gpi/beanartist/controleur/outils/OutilSélection.java
+++ src/fr/eseo/gpi/beanartist/controleur/outils/OutilSélection.java
@@ -19,7 +19,6 @@ public class OutilSélection extends Outil {
     public static final String NO_SELECTION_TEXT = "Aucune forme sélectionnée";
 
     private VueForme vueFormeSélectionnée;
-    private Forme formeSélectionnée = vueFormeSélectionnée.getForme();
     private ArrayList<AbstractSelectionAction> actions;
 
     public OutilSélection (PanneauDessin panneauDessin){
