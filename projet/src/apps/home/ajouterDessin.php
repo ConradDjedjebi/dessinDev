55369881d4d27b9968f510c7c88663dca99eb021
Antoine du Hamel <duhamelantoine1995@gmail.com>
2016-06-16 11:42:21 +0200

    Création de la page de soumission de dessin

---
+++ projet/src/apps/home/ajouterDessin.php
@@ -0,0 +1,34 @@
+<?php
+define('NO_CONNECTION_REQUIRED', 1);
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/dev/index.php';
+
+if(!isset($_GET['concours']))
+	redirect(ROOT_DIR);
+
+$page = new HTML\Doc('Soumettre un dessin');
+$page->addAPI('menu');
+$page->addAPI('form_handler');
+
+$page->body.= '<div id="page-content-wrapper">';
+
+
+$form = new HTML\Form(__DIR__.DIRECTORY_SEPARATOR.'gest_ajouterDessin.php');
+
+$form->addFieldset('Ajouter un dessin');
+	$form->hidden('concours', intval($_GET['concours']));
+	$form->input(['label'=>'Déposer le dessin', 'type'=>'file', 'name'=>'dessin','accept'=>'applicattion/xml']);
+	$form->hidden('MAX_FILE_SIZE', 1<<10<<10);
+
+	$form->input(['name'=>'juries[]', 'type'=>'select', 'multiple'=>true, 'other'=>[
+			'options'=>Prep::selectAll(['evaluateur', 'style'=>PDO::FETCH_COLUMN|PDO::FETCH_UNIQUE, 'argument'=>1]),
+			'help'=>'Vous devez choisir exactement deux jurys',
+			'label'=>'Choix des jurys',
+		]]);
+
+$form->submit('Soumettre');
+
+$page->body.= HTML::container('row', $form);
+
+$page->body.= '</div>';
+
