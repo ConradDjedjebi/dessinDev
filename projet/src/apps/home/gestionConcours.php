ed09ad3c4c19cb4acad4b6d2a164c2d801a7bc78
Antoine du Hamel <duhamelantoine1995@gmail.com>
2016-06-16 12:02:51 +0200

    Ajout de la page gestion de concours

---
+++ projet/src/apps/home/gestionConcours.php
@@ -0,0 +1,40 @@
+<?php
+define('NO_CONNECTION_REQUIRED', 1);
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/dev/index.php';
+
+if(!isset($_GET['concours']))
+	redirect(ROOT_DIR);
+
+$concours = Prep::selectOne(['concours', $_GET['concours'], 'field_ID'=>'numero']);
+
+$page = new HTML\Doc('Gérer le concours');
+
+$page->addAPI('UIcore');
+$page->addAPI('menu');
+
+// $page->addStyle('home', __DIR__);
+
+$page->body.= '<div id="page-content-wrapper">';
+
+$page->body.= HTML::container('row', 
+		HTML::h1 ('Administration du concours '.HTML::noXSS($concours['theme'].' ('.$concours['saison'].' '.$concours['annee'].')'))
+	);
+
+$menu = [
+    		'Ajouter un dessin' => '~apps/home/ajouterDessin.php',
+            'Ajouter une note' => '~apps/home/noter.php',
+            'Afficher les résultats' => '~apps/home/afficherResultats.php',
+    	];
+$liste = array();
+
+foreach ($menu as $label => $link)
+	$liste[]= HTML::a($link.'?concours='.$_GET['concours'], $label);
+
+	
+$page->body.= HTML::container('row', 
+		HTML::p('Vous pouvez accéder aux différentes sections pour ce concours :').
+		HTML::liste('ul', array(), $liste)
+	);
+
+$page->body.= '</div>';
