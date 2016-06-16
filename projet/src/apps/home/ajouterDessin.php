0dce1290f887c9a879fda525d87e2370fa14d035
Antoine du Hamel <duhamelantoine1995@gmail.com>
2016-06-16 12:02:38 +0200

    Ajout de la sécurité lors de la soumission d'un dessin

--- projet/src/apps/home/ajouterDessin.php
+++ projet/src/apps/home/ajouterDessin.php
@@ -12,21 +12,25 @@ $page->addAPI('form_handler');
 
 $page->body.= '<div id="page-content-wrapper">';
 
+try {
+	$form = new HTML\Form(__DIR__.DIRECTORY_SEPARATOR.'gest_ajouterDessin.php');
 
-$form = new HTML\Form(__DIR__.DIRECTORY_SEPARATOR.'gest_ajouterDessin.php');
+	$form->addFieldset('Ajouter un dessin');
+		$form->hidden('concours', intval($_GET['concours']));
+		$form->input(['label'=>'Déposer le dessin', 'type'=>'file', 'name'=>'dessin','accept'=>'applicattion/xml']);
+		$form->hidden('MAX_FILE_SIZE', 1<<10<<10);
+		$form->input(['label'=>'Date du dessin', 'name'=>'date_remise', 'type'=>'date']);
 
-$form->addFieldset('Ajouter un dessin');
-	$form->hidden('concours', intval($_GET['concours']));
-	$form->input(['label'=>'Déposer le dessin', 'type'=>'file', 'name'=>'dessin','accept'=>'applicattion/xml']);
-	$form->hidden('MAX_FILE_SIZE', 1<<10<<10);
+		$form->input(['name'=>'juries[]', 'type'=>'select', 'multiple'=>true, 'other'=>[
+				'options'=>Prep::selectAll(['evaluateur', 'style'=>PDO::FETCH_COLUMN|PDO::FETCH_UNIQUE, 'argument'=>1]),
+				'help'=>'Vous devez choisir exactement deux jurys',
+				'label'=>'Choix des jurys',
+			]]);
 
-	$form->input(['name'=>'juries[]', 'type'=>'select', 'multiple'=>true, 'other'=>[
-			'options'=>Prep::selectAll(['evaluateur', 'style'=>PDO::FETCH_COLUMN|PDO::FETCH_UNIQUE, 'argument'=>1]),
-			'help'=>'Vous devez choisir exactement deux jurys',
-			'label'=>'Choix des jurys',
-		]]);
-
-$form->submit('Soumettre');
+	$form->submit('Soumettre');
+} catch (prep\Exception $e) {
+	$form = HTML::container('alert alert-danger', 'Aucun jury n\'est inscrit, il n\'est pas possible d\'ajouter un dessin');
+}
 
 $page->body.= HTML::container('row', $form);
 
