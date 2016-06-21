f84067ce0dc19a1f56cc41cc0f3420f3168f76a4
Antoine du Hamel <duhamelantoine1995@gmail.com>
2016-06-21 11:14:21 +0200

    Ajout du script de création d'un concours

---
+++ projet/src/apps/concours/gest_create.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Antoine du HAMEL
+ * @category backend
+ * @package apps/connect
+ * @subpackage Formulaire_gestion
+ */
+define('NO_CONNECTION_REQUIRED', 1);
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/dev/index.php';
+use_file('date', PHPEXTENSION);
+
+$doc = new HTML\JSON;
+
+// $doc->exitError(print_r($_POST, true));
+// $doc->exitError(exist_plein('saison') ? 'true' : 'false');
+if (exist_plein('annee','saison', 'theme', 'date_debut', 'date_fin'))
+{
+	try {
+		Prep::insert('concours', ['annee', 'saison', 'theme', 'date_debut', 'date_fin'], $_POST);
+		$doc->redirect(__DIR__);
+		$doc->exitSuccess('Enregistré');
+	} catch (prep\Exception $e) {
+		$doc->exitError('Impossible d\'enregistrer le concours');
+	}
+}
+else
+{
+    $doc->exitError('Formulaire incomplet');
+}
